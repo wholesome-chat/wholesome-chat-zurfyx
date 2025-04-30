8c26e6810951775027538b761d735fcf80d6dc7c
Gerard Rovira <gerardrovira@meta.com>
2025-04-30 11:36:45 +0200

    ws server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,2 +1,20 @@
-const a: number = 1;
-console.info("hello world", a);
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+type Fruit = "APPLE" | "PEARS";
+
+wss.on("connection", (ws) => {
+  console.info("Client connected");
+
+  ws.on("message", (message) => {
+    console.info("Received:", message.toString());
+    ws.send(`Echo: ${message}`);
+  });
+
+  ws.on("close", () => {
+    console.info("Client disconnected");
+  });
+});
+
+console.info("WebSocket server is running on ws://localhost:8080");
